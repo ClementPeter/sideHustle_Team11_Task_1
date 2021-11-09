9027228de51b42c74ccaa60491ca9e8aa5111be4
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2021-11-09 23:43:13 +0100

    Refactored code

---
+++ version2.dart
@@ -0,0 +1,22 @@
+// SIDE HUSTLE INTERNSHIP COHORT 4
+// CAPSTONE PROJECT FOR TEAM 11 - MOBILE DEVS
+
+
+double studentAvg = 80;    //Variable to hold the student Average
+double classAvg = 65;     //Variable to hold the Class Average
+
+void main() {
+  
+  //Pass is calculated based on difference btw 
+  double pass = classAvg - studentAvg;
+  
+  
+  if (studentAvg>=60 && pass <= 5) {
+    print ("The Student Pass");
+  }
+  else {
+    print ("The Student failed");
+  }
+}
+
+
